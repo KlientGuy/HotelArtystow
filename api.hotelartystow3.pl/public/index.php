e5a91e818fced4c4f9104dc5b2639a47005d0a8a
KlientGuy <princeofnarf@gmail.com>
2024-06-23 21:07:18 +0200

    corsy w api

--- api.hotelartystow3.pl/public/index.php
+++ api.hotelartystow3.pl/public/index.php
@@ -2,6 +2,23 @@
 
 use App\Kernel;
 
+$allowed = [
+    'https://hotelartystow3.pl',
+    'https://www.hotelartystow3.pl',
+    'localhost:5001'
+];
+
+if(isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed))
+{
+    header("Access-Control-Allow-Origin: $_SERVER[HTTP_ORIGIN]");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type, withCredentials');
+
+    if($_SERVER['REQUEST_METHOD'] === 'OPTIONS')
+        die;
+}
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
