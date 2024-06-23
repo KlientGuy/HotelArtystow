b2f7b2b656375b58fca4e513ec45140c089e4d39
KlientGuy <princeofnarf@gmail.com>
2024-06-23 22:19:05 +0200

    zapomniałem se

---
+++ hotel_artystow_front/src/pages/Empty.svelte
@@ -0,0 +1,11 @@
+<style>
+    .full {
+        width: 100vw;
+        height: 100vh;
+        background-image: url("/svg/background.svg");
+        position: absolute;
+        z-index: 99;
+    }
+</style>
+<div class="full">
+</div>
